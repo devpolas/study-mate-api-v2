873e866fd8a0d687be2dd2017120f6e54288c9a0
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-13 20:02:57 +0600

    fix typeo

--- models/userModel.js
+++ models/userModel.js
@@ -57,7 +57,7 @@ const userSchema = new mongoose.Schema(
       },
       default: "student",
     },
-    profileImage: String,
+    image: String,
     subject: String,
     studyMode: {
       type: Boolean,
